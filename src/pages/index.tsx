ab296bddcf560413aaf76711be4dada590b4fab6
Gabriel Fernandes Rosa Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-02-25 17:20:17 -0300

    End of lesson 4
    Added the last touches to context management and finished making
    all the features functional

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,26 @@ import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from '../components/ChallengeBox';
+import { CountdownProvider } from '../contexts/CountdownContext';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>
   )
 }
