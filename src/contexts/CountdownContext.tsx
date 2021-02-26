0adf8afaca61b7c9baf5823087299c289f4165cd
Gabriel Fernandes Rosa Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-02-26 19:17:56 -0300

    Corrected cicle time back to 25 minutes

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -21,7 +21,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const ciclePeriod = .1 * 60;
+  const ciclePeriod = 25 * 60;
 
   const [time, setTime] = useState(ciclePeriod);
   const [isActive, setIsActive] = useState(false);
